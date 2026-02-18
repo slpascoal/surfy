c79bc64ba9ff89da4fd3b485cfbffd5fb85b199b
Silas L. Pascoal <slpascoal01@gmail.com>
2026-02-18 20:16:22 +0000

    adicionando agendamento componente

--- components/home/main.tsx
+++ components/home/main.tsx
@@ -1,6 +1,9 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { SearchIcon } from "lucide-react";
+import { Card, CardContent } from "../ui/card";
+import { Badge } from "../ui/badge";
+import { Avatar, AvatarImage } from "../ui/avatar";
 
 export default function Main() {
   return (
@@ -15,6 +18,28 @@ export default function Main() {
             <SearchIcon />
           </Button>
         </div>
+
+        <Card className="homeAgendamentos">
+          <CardContent className="agendamentosContent">
+            <div className="agendamentosDetails">
+              <Badge>Confirmado</Badge>
+              <h3>Corte de Cabelo</h3>
+
+              <div className="agendamentosCompany">
+                <Avatar>
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"/>
+                </Avatar>
+                <p>Barbearia Teste</p>
+              </div>
+            </div>
+
+            <div className="agendamentosDate">
+              <p className="text-sm">Fevereiro</p>
+              <p className="text-2xl">05</p>
+              <p className="text-sm">16:30</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </main>
   );
